c31b5e8014c209133a29911e909b53bf213de51a
Daniel Tsai <danieltsai.developer@gmail.com>
2017-08-29 10:38:25 -0700

    Add configureStore.js

---
+++ src/configureStore.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import rootReducer from './reducers'
+
+const loggerMiddleware = createLogger({
+    level: 'info',
+    collapsed: true
+})
+
+const createStoreWithMiddleware = process.env.NODE_ENV === 'development'
+    ? applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )(createStore)
+    : applyMiddleware(thunkMiddleware)(createStore)
+
+export default function configureStore(initialState) {
+    const store = createStoreWithMiddleware(rootReducer, initialState)
+
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('./reducers', () => {
+            const nextRootReducer = require('./reducers/index')
+            store.replaceReducer(nextRootReducer)
+        })
+    }
+
+    return store
+}
