9c78f5c1cc81952c680dae89ea42f5d63a7bd540
Daniel Tsai <danieltsai.developer@gmail.com>
2017-12-07 11:55:38 -0800

    Update jest to use React 16

--- src/actions/reactStarterActions.js
+++ src/actions/reactStarterActions.js
@@ -3,8 +3,8 @@ import * as ActionTypes from 'constants/ActionTypes'
 import * as API from 'server/api'
 
 export function getExample() {
-    return dispatch => (
-        API.getExample().then(
+    return dispatch => {
+        return API.getExample().then(
             response => {
                 dispatch(displayToast(response.body.message, toast.TYPE.SUCCESS))
             },
@@ -12,7 +12,7 @@ export function getExample() {
                 dispatch(displayToast(JSON.parse(error.response.text).errors[0].detail, toast.TYPE.ERROR))
             }
         )
-    )
+    }
 }
 
 export function displayToast(message, toastType) {
